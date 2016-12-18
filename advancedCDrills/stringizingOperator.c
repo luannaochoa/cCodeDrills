bfd580a297c6f91abaa8fbdd4a9688e6d74ee3aa
Luanna Ochoa <lu@earth.attlocal.net>
2016-12-18 12:33:45 -0500

    update

--- advancedCDrills/stringizingOperator.c
+++ advancedCDrills/stringizingOperator.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+//# is one of three preprocessor operators 
+//the other 2 operators are the token pasting operator ### and the define operator
+//the behavior is the same for both C and C++ compilers 
+
+#define getVariableName(x) #x
+
+int main(void){
+
+	int student_age=21;
+	printf("Value of %s is = %d\n", getVariableName(student_age), student_age );
+	
+	return 0;
+}
